284cab43689e6f168e9ede83a6b048d6de6e882b
Cobertura360Pro <ml@baumann.com.br>
2026-03-30 23:09:11 -0300

    Create LeadInteraction.php

---
+++ app/Models/LeadInteraction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LeadInteraction extends Model
+{
+    protected $fillable = [
+        'lead_id',
+        'tipo',
+        'conteudo',
+    ];
+
+    public function lead(): BelongsTo
+    {
+        return $this->belongsTo(Lead::class);
+    }
+}
