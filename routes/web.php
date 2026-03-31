407d9a6a0d3e53bb184fb0198ad292ca475db1a4
Cobertura360Pro <ml@baumann.com.br>
2026-03-30 21:25:27 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes - Lead360 AI
-|--------------------------------------------------------------------------
-*/
-
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/painel', function () {
     return view('painel.index');
-});
+})->name('painel.index');
 
 Route::get('/leads', function () {
     return view('leads.index');
-});
+})->name('leads.index');
 
 Route::get('/conversas', function () {
     return view('conversas.index');
-});
+})->name('conversas.index');
 
 Route::get('/configuracoes', function () {
     return view('configuracoes.index');
-});
+})->name('configuracoes.index');
