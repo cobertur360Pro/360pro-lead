b2c937df1a85410041f27495e164ddb2cea64490
Cobertura360Pro <ml@baumann.com.br>
2026-03-31 16:11:27 -0300

    Update Parametro.php

--- app/Models/Parametro.php
+++ app/Models/Parametro.php
@@ -1,1 +1,32 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Parametro extends Model
+{
+    protected $table = 'parametros';
+
+    protected $fillable = [
+        'codigo',
+        'nome',
+        'descricao',
+        'bloco',
+        'camada',
+        'escopo',
+        'tipo',
+        'valor_padrao',
+        'ativo',
+    ];
+
+    protected $casts = [
+        'ativo' => 'boolean',
+    ];
+
+    public function valores(): HasMany
+    {
+        return $this->hasMany(ParametroValor::class);
+    }
+}
