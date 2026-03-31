26bc9157ee7de242bd18fe0601bd47cbc86d9013
Cobertura360Pro <ml@baumann.com.br>
2026-03-30 21:00:56 -0300

    Create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lead360 AI</title>
+</head>
+<body>
+    <h1>Lead360 AI</h1>
+    <p>Sistema inicial rodando.</p>
+
+    <ul>
+        <li><a href="/painel">Painel</a></li>
+        <li><a href="/leads">Leads</a></li>
+        <li><a href="/conversas">Conversas</a></li>
+        <li><a href="/configuracoes">Configurações</a></li>
+    </ul>
+</body>
+</html>
