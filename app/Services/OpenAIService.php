32150c1088051725cb5c8daee05238571f382606
Cobertura360Pro <ml@baumann.com.br>
2026-03-31 00:15:59 -0300

    Update OpenAIService.php

--- app/Services/OpenAIService.php
+++ app/Services/OpenAIService.php
@@ -30,7 +30,8 @@ class OpenAIService
                         'content' => $mensagem,
                     ],
                 ],
-                'temperature' => 0.7,
+                'temperature' => 0.6,
+                'max_tokens' => 300,
             ]);
 
         if (! $response->successful()) {
